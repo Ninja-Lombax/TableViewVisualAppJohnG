9627354ce58c390c5a78773b924d13f1388856db
Ninja-Lombax <godzillakotm80@gmail.com>
2020-10-04 16:09:47 -0500

    Initial commit

--- TableViewVisualAppJohnG/ViewController.swift
+++ TableViewVisualAppJohnG/ViewController.swift
@@ -8,11 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let petArray = ["cat","dog","parakeet","canary","finch","tropical fish","goldfish","sea horses","hamster","gerbil","rabbit","turtle","snake","lizard","hermit crab","snake","bat","wolf","fox","parrot"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section:Int) -> Int{
+        
+        return petArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellID")
+        
+        cell?.textLabel?.text = petArray[indexPath.row]
+        
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+            {
+        let selectedItem = petArray[indexPath.row]
+        let alert = UIAlertController(title: "Your Choice", message: "\(selectedItem)", preferredStyle: .alert)
+        let okAction = UIAlertAction(title:"Ok", style: .default, handler: {action -> Void in})
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
     }
 
 
